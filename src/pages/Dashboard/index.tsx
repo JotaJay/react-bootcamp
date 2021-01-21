92ee5ef6400321417a90735ff7d1978eee26b99e
Junior <reinaldo.diasjunior@gmail.com>
2021-01-21 17:23:40 -0300

    Adds Repositories component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,49 +1,55 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './style';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: { login: string; avatar_url: string };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositores] = useState<Repository[]>([]);
+
+  async function handleAddRepo(e: FormEvent<HTMLFormElement>): Promise<void> {
+    e.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositores([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explore Github Repositories</Title>
 
-      <Form>
-        <input placeholder="Search a repository" />
+      <Form onSubmit={handleAddRepo}>
+        <input
+          placeholder="Search a repository"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Search</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/40603427?s=460&u=a1563b4d0775c3031bda3263fac42ac22a4a61aa&v=4"
-            alt="Junior profile pic"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-              cumque!
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/40603427?s=460&u=a1563b4d0775c3031bda3263fac42ac22a4a61aa&v=4"
-            alt="Junior profile pic"
-          />
-          <div>
-            <strong>rocketseat/unform</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
-              cumque!
-            </p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repo => (
+          <a key={repo.full_name} href="teste">
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
