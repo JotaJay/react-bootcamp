4612e0b309d2ca6211beda68c5427af7760266fc
Junior <reinaldo.diasjunior@gmail.com>
2021-01-21 11:19:13 -0300

    Adds style to list items

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import logo from '../../assets/logo.svg';
-import { Title, Form } from './style';
+import { Title, Form, Repositories } from './style';
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,6 +13,22 @@ const Dashboard: React.FC = () => {
         <input placeholder="Search a repository" />
         <button type="submit">Search</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/40603427?s=460&u=a1563b4d0775c3031bda3263fac42ac22a4a61aa&v=4"
+            alt="Junior profile pic"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero,
+              cumque!
+            </p>
+          </div>
+        </a>
+      </Repositories>
     </>
   );
 };
