10e188a5447f0209250bf531610644af22944889
Junior <reinaldo.diasjunior@gmail.com>
2021-01-21 11:03:52 -0300

    Adds style to Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-import { Title } from './style';
+import logo from '../../assets/logo.svg';
+import { Title, Form } from './style';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore github repositories</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore Github Repositories</Title>
+
+      <Form>
+        <input placeholder="Search a repository" />
+        <button type="submit">Search</button>
+      </Form>
+    </>
+  );
 };
 
 export default Dashboard;
