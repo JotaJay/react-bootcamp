8c83abc22020a4dd8097e19e11b3b7f87e722e16
Junior <reinaldo.diasjunior@gmail.com>
2021-01-26 10:49:58 -0300

    Adds dynamic content @Repository page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import { Profile, Header, Metrics, Issues } from './style';
 import logo from '../../assets/logo.svg';
@@ -9,66 +10,87 @@ interface QueryParams {
   repository: string;
 }
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: { login: string; avatar_url: string };
+  forks_count: number;
+  open_issues_count: number;
+  stargazers_count: number;
+}
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<QueryParams>();
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, Setissues] = useState('');
+
+  useEffect(() => {
+    const result = api
+      .get<Repository>(`/repos/${params.repository}`)
+      .then(response => {
+        console.log(response.data);
+        setRepository(response.data);
+      });
+  }, [params.repository]);
   return (
     <>
       <Header>
-        <img src={logo} alt="" />
+        <img src={logo} alt="Github Explorer" />
         <Link to="/">
           <FiChevronLeft size={16} />
           Go back
         </Link>
       </Header>
-      <Profile>
-        <img
-          src="https://avatars.githubusercontent.com/u/40603427?s=460&u=a1563b4d0775c3031bda3263fac42ac22a4a61aa&v=4"
-          alt="joao"
-        />
-        <div>
-          <strong>Teste</strong>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-      </Profile>
-      <Metrics>
-        <ul>
-          <li>
-            <strong>15422</strong>
-            <span>Stars</span>
-          </li>
-          <li>
-            <strong>48</strong>
-            <span>Forks</span>
-          </li>
-          <li>
-            <strong>67</strong>
-            <span>Issues abertas</span>
-          </li>
-        </ul>
-      </Metrics>
-      <Issues>
-        <Link key="teste" to="/repositories">
-          <div>
-            <strong>issue</strong>
-            <p>Issue description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link key="teste" to="/repositories">
-          <div>
-            <strong>issue</strong>
-            <p>Issue description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-        <Link key="teste" to="/repositories">
-          <div>
-            <strong>issue</strong>
-            <p>Issue description</p>
-          </div>
-          <FiChevronRight size={20} />
-        </Link>
-      </Issues>
+      {repository && (
+        <>
+          <Profile>
+            <img src={`${repository?.owner.avatar_url}`} alt="joao" />
+            <div>
+              <strong>{repository?.full_name}</strong>
+              <p>{repository?.description}</p>
+            </div>
+          </Profile>
+          <Metrics>
+            <ul>
+              <li>
+                <strong>{repository.stargazers_count}</strong>
+                <span>Stars</span>
+              </li>
+              <li>
+                <strong>{repository.forks_count}</strong>
+                <span>Forks</span>
+              </li>
+              <li>
+                <strong>{repository.open_issues_count}</strong>
+                <span>Open Issues</span>
+              </li>
+            </ul>
+          </Metrics>
+          <Issues>
+            <Link key="teste" to="/repositories">
+              <div>
+                <strong>issue</strong>
+                <p>Issue description</p>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+            <Link key="teste" to="/repositories">
+              <div>
+                <strong>issue</strong>
+                <p>Issue description</p>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+            <Link key="teste" to="/repositories">
+              <div>
+                <strong>issue</strong>
+                <p>Issue description</p>
+              </div>
+              <FiChevronRight size={20} />
+            </Link>
+          </Issues>
+        </>
+      )}
     </>
   );
 };
